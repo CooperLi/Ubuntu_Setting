cdeae87c3c2af3be6d3e297c621e7edffceab1fb
CooperLi <a710905118@gmail.com>
2016-11-05 23:20:15 +0800

    Add files via upload

---
+++ Dependence.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+sudo apt-get install -y libopencv-dev build-essential checkinstall cmake pkg-config yasm libtiff4-dev libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils libgtk2.0-dev
